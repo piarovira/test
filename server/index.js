28947a40b9d3cb02d475a7ddaa34f5c6d3d29354
Pia Rovira <piarovira98@gmail.com>
2022-06-27 17:23:45 -0300

    first

--- server/index.js
+++ server/index.js
@@ -6,12 +6,18 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
+app.get('/', (req, res) => {
+	console.log('PIPIPIPIPIPIPI');
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({ greeting: `Hello!` }));
+});
+
 app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+	const name = req.query.name || 'World';
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
 app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+	console.log('Express server is running on localhost:3001')
 );
